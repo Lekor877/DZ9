dcfd328a209fee6a5281751e75726e06809d16f6
Maxim Pomiskit <pomiskiy_m@mail.ru>
2022-12-01 18:01:43 +0500

    +

---
+++ dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+Console.Write("Введи M: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
